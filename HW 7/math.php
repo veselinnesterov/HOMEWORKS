204e5b5a93e9b9bfb96b21c5b4fb7f4c63c92d62
Veselin Nesterov <vesko91@gmail.com>
2016-03-22 18:09:05 +0200

    hw 7 ver 2

---
+++ HW 7/math.php
@@ -0,0 +1,22 @@
+<?php
+    
+	$a=5;
+	$b=10;
+	$c=12;
+	$d=4;
+	$e=9;
+	$f=0;
+	$g=-5;
+	$h=0;
+	$i=0.2;
+	$j=20;
+	$k=9;
+	$l=0;
+
+    var_dump($a === $g);
+	var_dump ($b != $h);
+	var_dump ($c <> $i);
+    var_dump ($d >= $j);
+    var_dump ($e == $k);
+    var_dump ($f < $l);
+?>
---
+++ HW 7/url.php
@@ -0,0 +1,11 @@
+<?php
+
+$url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+echo $url;
+
+
+
+
+
+
+?>
